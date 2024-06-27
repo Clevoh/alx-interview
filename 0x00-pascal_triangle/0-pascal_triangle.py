7601403e7ae7a13dc590dddaef884e6baab7c086
Cleve <clevelihasi@gmail.com>
2024-06-27 21:07:24 +0200

    0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def pascal_triangle(n):
+    '''
+    Generate Pascal's triangle up to the nth row.
+    '''
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        temp = [1]
+        for j in range(1, i):
+            temp.append(triangle[i-1][j-1] + triangle[i-1][j])
+        temp.append(1)
+        triangle.append(temp)
+
+    return triangle
