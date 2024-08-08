b85c140ac3727b3c799ddab791e39b80dd8f0c30
Clevoh <clevelihasi@gmail.com>
2024-08-08 15:07:50 +0000

    0. Star Wars Characters

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+const request = require('request');
+const API_URL = 'https://swapi-api.alx-tools.com/api';
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    let film;
+    try {
+      film = JSON.parse(body);
+    } catch (parseErr) {
+      console.log('Invalid JSON response');
+      return;
+    }
+
+    if (!film || !film.characters) {
+      console.log('Movie not found or invalid movie ID');
+      return;
+    }
+
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
