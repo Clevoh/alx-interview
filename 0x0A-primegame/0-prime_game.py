70f1816ccbd02073162d6356dadfaf8d3162f1ae
Clevoh <clevelihasi@gmail.com>
2024-09-07 09:09:06 +0000

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,13 +3,18 @@
 
 
 def isWinner(x, nums):
-    """Function that determines who has won"""
+    """Determines the winner between Maria and Ben."""
+    if x < 1 or not nums:
+        return None
+
     mariaWinsCount = 0
     benWinsCount = 0
+    max_num = max(nums)
+    primes = sieve_of_eratosthenes(max_num)
 
     for num in nums:
         roundsSet = list(range(1, num + 1))
-        primesSet = primes_in_range(1, num)
+        primesSet = [p for p in primes if p <= num]
 
         if not primesSet:
             benWinsCount += 1
@@ -17,41 +22,35 @@ def isWinner(x, nums):
 
         isMariaTurns = True
 
-        while(True):
-            if not primesSet:
-                if isMariaTurns:
-                    benWinsCount += 1
-                else:
-                    mariaWinsCount += 1
-                break
-
+        while primesSet:
             smallestPrime = primesSet.pop(0)
             roundsSet.remove(smallestPrime)
-
             roundsSet = [x for x in roundsSet if x % smallestPrime != 0]
 
             isMariaTurns = not isMariaTurns
 
+        # Determine the winner of this round
+        if isMariaTurns:
+            benWinsCount += 1
+        else:
+            mariaWinsCount += 1
+
+    # Determine overall winner
     if mariaWinsCount > benWinsCount:
-        return "Winner: Maria"
-
-    if mariaWinsCount < benWinsCount:
-        return "Winner: Ben"
-
+        return "Maria"
+    elif benWinsCount > mariaWinsCount:
+        return "Ben"
     return None
 
 
-def is_prime(n):
-    """Returns True if n is prime, else False."""
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+def sieve_of_eratosthenes(limit):
+    """Returns a list of all primes up to the given limit."""
+    sieve = [True] * (limit + 1)
+    sieve[0] = sieve[1] = False  # 0 and 1 are not prime numbers
 
+    for start in range(2, int(limit**0.5) + 1):
+        if sieve[start]:
+            for multiple in range(start * start, limit + 1, start):
+                sieve[multiple] = False
 
-def primes_in_range(start, end):
-    """Returns a list of prime numbers."""
-    primes = [n for n in range(start, end+1) if is_prime(n)]
-    return primes
+    return [num for num, is_prime in enumerate(sieve) if is_prime]
